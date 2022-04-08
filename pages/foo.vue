029decbce2f91f1d693c37a74dd362aac77d69bb
Vishnu Jayadevan <me@vishnujayadevan.com>
2022-04-08 08:24:10 +0530

    bootstrap nuxt 3 + tailwind

---
+++ pages/foo.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="uppercase">TEST</h1>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    // "./nuxt.config.{js,ts}",
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
